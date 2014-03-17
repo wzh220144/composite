abbdd7128cbc2fcb20f01f5fb48c488f335977d3
cos <cos@ubuntu.(none)>
2014-03-17 12:39:57 -0400

    try to add the library and header file of sqlite3

--- src/components/implementation/tests/unit_posix/posix_test.c
+++ src/components/implementation/tests/unit_posix/posix_test.c
@@ -1,5 +1,6 @@
 #include <cos_component.h>
 #include <stdio.h>
+#include <sqlite3.h>
 
 /* TODO: added micro benchmark */ 
 
@@ -7,5 +8,8 @@ void
 cos_init(void *args)
 {
 /* use posix functions or system calls ?*/ 
+	sqlite3 *db;
+	fprintf(stdout, "Fuck!!!\n");
+	int rc = sqlite3_open(":memory", &db);	
         return;
 }
